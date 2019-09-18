06c2afa7a12b5e0c0424c8bc544a3e11663fcc7b
Benjamin Schultz Larsen <benjaco1996+edu@gmail.com>
2019-09-18 16:22:00 +0200

    javadocs for get car list dto's

--- src/main/java/DTO/listResponse.java
+++ src/main/java/DTO/listResponse.java
@@ -4,7 +4,17 @@ import Entities.Car;
 
 import java.util.List;
 
+/**
+ * listResponse
+ * This is the data transmission object used when responding to information regarding available rental cars
+ * Used as return type for GetAvailableCars
+ * @since 1.0
+ */
 public class listResponse
 {
+    /**
+     * Used as container for all available rental cars
+     * @since 1.0
+     */
     List<Car> cars;
 }
