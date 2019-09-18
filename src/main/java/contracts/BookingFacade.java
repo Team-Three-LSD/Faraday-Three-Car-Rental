3695420dc56fb93db6aa73002c17f51b44072fb1
Stanislav Novitski <Stanislav.Novitski@ibm.com>
2019-09-18 21:10:46 +0200

    added minor comments to bookign facade

--- src/main/java/contracts/BookingFacade.java
+++ src/main/java/contracts/BookingFacade.java
@@ -17,11 +17,15 @@ public class BookingFacade implements IBooking {
 
     @Override
     public BookingDTO addNewBooking(String carType, String pickUpCity, String pickUpLocation, String deliveryLocation, String startDate, String endDate) throws Exception {
+        // DO database stuff and return the resulting booking if success else throw errors
+
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public List<BookingDTO> searchBooking(String driverLicenseNumber) throws Exception {
+        // DO database stuff and return the resulting booking if success else throw errors
+
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
