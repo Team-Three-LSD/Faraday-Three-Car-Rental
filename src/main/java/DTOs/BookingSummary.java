ea60a51c1bf54ef90252690759f194d7d0656eec
Elitsa <elit0451@edu.eal.dk>
2019-09-30 06:29:50 +0200

    Added a BookingSummary DTO for shorter descriptions

---
+++ src/main/java/DTOs/BookingSummary.java
@@ -0,0 +1,21 @@
+package DTOs;
+
+import DTOs.Identifiers.BookingIdentifier;
+import DTOs.Identifiers.CarIdentifier;
+import DTOs.Identifiers.StationIdentifier;
+
+import java.time.LocalDateTime;
+
+public class BookingSummary extends BookingIdentifier {
+
+    private LocalDateTime pickUpTime;
+    private StationIdentifier pickUpPlace;
+    private CarIdentifier car;
+
+    public BookingSummary(LocalDateTime pickUpTime, StationIdentifier pickUpPlace, CarIdentifier car, String bookingId) {
+        super(bookingId);
+        this.pickUpTime = pickUpTime;
+        this.pickUpPlace = pickUpPlace;
+        this.car = car;
+    }
+}
